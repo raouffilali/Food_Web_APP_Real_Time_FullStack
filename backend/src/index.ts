0288cb669fad9d944612923cc105cc194429cc22
Abderraouf FILALI <filaliabderaouf@yahoo.com>
2024-02-22 12:49:53 +0100

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/test", async (req: Request, res: Response) => {
+  res.json({ message: "Hello World" });
+});
+
+app.listen(7000, () => {
+   mongoose
+     .connect(process.env.MONGODB_CONNECTION_STRING as string)
+     .then(() => {
+       console.log("Connected to MongoDB");
+     })
+     .catch((error) => {
+       console.log("Error connecting to MongoDB: ", error.message);
+     });
+  console.log("Server is running on port 7000");
+});
