5296af51cdedeff79ef8ebe32cb1d78e6effb07a
Abderraouf FILALI <filaliabderaouf@yahoo.com>
2024-02-22 13:36:10 +0100

    Setting up Layout and Header

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,9 +1,10 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./layouts/Layout";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<div>Home</div>} />
+      <Route path="/" element={<Layout>HomePage</Layout>} />
       <Route path="/user" element={<div>User Page</div>} />
       <Route path="/admin" element={<div>Admin Page</div>} />
       <Route path="*" element={<Navigate to="/" />} />
