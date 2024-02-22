0d64ab3f0918bc76ce955f26e25548a8b36cebb2
Abderraouf FILALI <filaliabderaouf@yahoo.com>
2024-02-22 14:04:42 +0100

    Add MainNav component for larger screen

---
+++ frontend/src/components/MainNav.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./ui/button";
+const MainNav = () => {
+  return (
+    <Button
+      variant={"ghost"}
+      className="font-bold hover:text-orange-500 hover:bg-white text-xl"
+    >
+      Sing In
+    </Button>
+  );
+};
+
+export default MainNav;
